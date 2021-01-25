9f964dc507cfdf668de312373689ccc3c6880b73
SpyrosGeo <xarodasimba@gmail.com>
2021-01-25 17:11:55 +0200

    added Item component and basic Grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,57 @@
-import { useState} from 'react'
-import { useQuery} from 'react-query'
+import { useState } from "react";
+import { useQuery } from "react-query";
 
 //Components
-import Drawer from '@material-ui/core/Drawer'
-import LinearProgress from '@material-ui/core/LinearProgress'
-import Grid from '@material-ui/core/Grid'
-import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
-import Badge from '@material-ui/core/Badge'
+import Item from './Item/Item'
+import Drawer from "@material-ui/core/Drawer";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Grid from "@material-ui/core/Grid";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import Badge from "@material-ui/core/Badge";
 
 //Styles
-import { } from './App.styles'
-
+import {Wrapper} from "./App.styles";
 
 //types
 export type CartItemType = {
-  id:number;
-  category:string;
-  description:string;
-  image:string;
-  price:number;
-  title:string;
-  amount:number;
-}
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
 
-const getProducts = async (): Promise<CartItemType[]> =>(
- await(await fetch("https://fakestoreapi.com/products")).json()
-)
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch("https://fakestoreapi.com/products")).json();
 
 const App = () => {
-  const {data,isLoading,error } = useQuery<CartItemType[]>('products',getProducts)
-  console.log(data)
-  return <div className="App">Start</div>;
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "products",
+    getProducts
+  );
+
+  const getTotalItems = () => null;
+
+  const handleAddToCart = (clickedItem:CartItemType) => null;
+
+  const handleRemoveFromCart = () =>null;
+
+if (isLoading) return <LinearProgress/>;
+if(error) return <div>Something Went Wrong</div>
+
+  return (
+    <Wrapper>
+      <Grid container spacing={3}>
+    {data?.map((item=>(
+      <Grid item key={item.id} xs={12} sm={4}>
+        <Item item={item} handleAddToCart={handleAddToCart}/>
+      </Grid>
+    )))}
+      </Grid>
+    </Wrapper>
+  )
 };
 
 export default App;
