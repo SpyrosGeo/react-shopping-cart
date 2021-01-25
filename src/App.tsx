87a8f20328a4ee63d9db6ab5664f9bfde12dd928
SpyrosGeo <xarodasimba@gmail.com>
2021-01-25 18:03:17 +0200

    finished the cart layout

--- src/App.tsx
+++ src/App.tsx
@@ -37,8 +37,8 @@ const App = () => {
     getProducts
   );
 
-  const getTotalItems = (items:CartItemType[]) => {
-    return items.reduce((ack:number,item) => ack + item.amount, 0 )
+  const getTotalItems = (items: CartItemType[]) => {
+    return items.reduce((ack: number, item) => ack + item.amount, 0);
   };
 
   const handleAddToCart = (clickedItem: CartItemType) => null;
@@ -51,11 +51,15 @@ const App = () => {
   return (
     <Wrapper>
       <Drawer anchor="right" open={cartOpen} onClose={() => setCartOpen(false)}>
-       <Cart cartItems={cartItems} addToCart={handleAddToCart} removeFromCart={handleRemoveFromCart}/>
+        <Cart
+          cartItems={cartItems}
+          addToCart={handleAddToCart}
+          removeFromCart={handleRemoveFromCart}
+        />
       </Drawer>
       <StyledButton onClick={() => setCartOpen(true)}>
         <Badge badgeContent={getTotalItems(cartItems)} color="error">
-          <AddShoppingCartIcon/>
+          <AddShoppingCartIcon />
         </Badge>
       </StyledButton>
 
