226db90fc3ed15265aa35917f15b0030aebf4cb2
SpyrosGeo <xarodasimba@gmail.com>
2021-01-25 18:27:02 +0200

    added full functionality for adding and removing to cart and also displaying the total

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -14,6 +14,11 @@ type Props = {
 
 
 const Cart:React.FC<Props>=({cartItems,addToCart,removeFromCart})=>{
+
+    const calculateTotal = (items:CartItemType[])=>
+        items.reduce((ack:number,item)=>ack + item.amount * item.price,0)
+
+
     return (
         <Wrapper>
             <h2>Shopping Cart</h2>
@@ -28,6 +33,7 @@ const Cart:React.FC<Props>=({cartItems,addToCart,removeFromCart})=>{
                 
                 />
             ))}
+            <h2>Total: ${calculateTotal(cartItems).toFixed(2)}</h2>
         </Wrapper>
     )
 }
